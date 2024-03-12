d87501e4ef05b62bca671633dba60d0665ec302d
robert0004 <6243646+robert0004@users.noreply.github.com>
2024-03-11 18:32:03 -0700

    time zone fixed

--- 161Bot/SlashCommands/DaysRemaining.cs
+++ 161Bot/SlashCommands/DaysRemaining.cs
@@ -16,15 +16,15 @@ namespace _161Bot.SlashCommands
         public async Task Run(SocketSlashCommand cmd)
         {
                 var l = (long)BotConfig.GetCachedConfig().CurrentTermEnds;
-                DateTime d1 = DateTimeOffset.FromUnixTimeSeconds(l).DateTime;
-                DateTime d2 = DateTime.Now;
+                DateTimeOffset d1 = DateTimeOffset.FromUnixTimeSeconds(l);
+                DateTimeOffset d2 = DateTimeOffset.Now;
                 TimeSpan diff = d1 - d2;
                 await cmd.RespondAsync(embed: new EmbedBuilder().WithTitle("Days Until End of " + BotConfig.GetCachedConfig().CurrentTerm).WithDescription(
                     "Days: " + diff.Days.ToString() + "\n" +
                     "+ Hours: " + diff.Hours.ToString() + "\n" +
                     "+ Minutes: " + diff.Minutes.ToString() + "\n" +
                     "+ Seconds: " + diff.Seconds.ToString() + "\n"
-                    ).WithColor(Color.Blue).WithFooter(BotConfig.GetCachedConfig().CurrentTerm + " ends at: " + d1.ToString() + " • Time on Server: " + d2.ToString()).Build());
+                    ).WithColor(Color.Blue).WithFooter(BotConfig.GetCachedConfig().CurrentTerm + " ends at: " + d1.ToLocalTime().ToString() + " • Time on Server: " + d2.ToLocalTime().ToString()).Build());
 
 
         }
